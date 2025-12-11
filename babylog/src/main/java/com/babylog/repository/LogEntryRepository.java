22fc4e7f4bd8487117914aa969f40130461738cc
Bracha Horvitz <101530967+BrachaHorowitz@users.noreply.github.com>
2025-12-11 18:47:52 +0200

    feat: Add LogEntryRepository interface

---
+++ babylog/src/main/java/com/babylog/repository/LogEntryRepository.java
@@ -0,0 +1,10 @@
+package com.babylog.repository;
+
+import com.babylog.entity.LogEntry;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LogEntryRepository extends JpaRepository<LogEntry, String> {
+
+}
