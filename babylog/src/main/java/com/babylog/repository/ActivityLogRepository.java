c9db357f4d4555abfd7e89e6c2ca8e22ff20ab18
Bracha Horvitz <101530967+BrachaHorowitz@users.noreply.github.com>
2025-12-14 23:13:48 +0200

    Refactor: rename repository to ActivityLogRepository

--- babylog/src/main/java/com/babylog/repository/ActivityLogRepository.java
+++ babylog/src/main/java/com/babylog/repository/ActivityLogRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LogEntryRepository extends JpaRepository<ActivityLog, String> {
+public interface ActivityLogRepository extends JpaRepository<ActivityLog, String> {
 
 }
