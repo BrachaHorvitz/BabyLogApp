8a79fd75f81165d752756797e5e85b0cb340bf4d
Bracha Horvitz <101530967+BrachaHorowitz@users.noreply.github.com>
2025-12-14 22:46:30 +0200

    Refactor: rename LogEntry entity to ActivityLog

--- babylog/src/main/java/com/babylog/entity/ActivityLog.java
+++ babylog/src/main/java/com/babylog/entity/ActivityLog.java
@@ -16,7 +16,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class LogEntry {
+public class ActivityLog {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
--- babylog/src/main/java/com/babylog/repository/LogEntryRepository.java
+++ babylog/src/main/java/com/babylog/repository/LogEntryRepository.java
@@ -1,10 +1,10 @@
 package com.babylog.repository;
 
-import com.babylog.entity.LogEntry;
+import com.babylog.entity.ActivityLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LogEntryRepository extends JpaRepository<LogEntry, String> {
+public interface LogEntryRepository extends JpaRepository<ActivityLog, String> {
 
 }
